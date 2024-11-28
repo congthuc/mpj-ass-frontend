ed363512cd943335e0448a88e983bf7a69636659
congthucphan <congthuc.uit@gmail.com>
2024-11-28 16:18:56 +0700

    git push code changes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { StudentListComponent } from './student-list/student-list.component';
+import { CreateStudentComponent } from './create-student/create-student.component';
+import { UpdateStudentComponent } from './update-student/update-student.component';
+import { StudentDetailsComponent } from './student-details/student-details.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: 'students', component: StudentListComponent},
+  {path: 'create-student', component: CreateStudentComponent},
+  {path: '', redirectTo: 'students', pathMatch: 'full'},
+  {path: 'update-student/:id', component: UpdateStudentComponent},
+  {path: 'student-details/:id', component: StudentDetailsComponent}];
